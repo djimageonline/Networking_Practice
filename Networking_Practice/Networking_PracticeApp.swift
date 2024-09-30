bb6143223efe12a610ab8936d0eb8f2f0573cbca
Johnny Proano <jproano22@yahoo.com>
2024-09-30 16:22:41 -0400

    Initial Commit

---
+++ Networking_Practice/Networking_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_PracticeApp.swift
+//  Networking_Practice
+//
+//  Created by Johnny Proano on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Networking_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
